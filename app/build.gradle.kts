c87cca005478a826c645a82188cebac1768b7cab
Rafi Panoyan <rafi.panoyan@hotmail.fr>
2020-04-18 11:12:42 +0200

    add runtime dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,19 +38,26 @@ android {
     buildFeatures {
         compose = true
     }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "0.1.0-dev09"
+    }
 }
 
 dependencies {
     val uiVersion = "0.1.0-dev09"
+    val composeVersion = "0.1.0-dev09"
 
     implementation(kotlin("stdlib", org.jetbrains.kotlin.config.KotlinCompilerVersion.VERSION))
     implementation("androidx.core:core-ktx:1.2.0")
     implementation("androidx.appcompat:appcompat:1.1.0")
     implementation("com.google.android.material:material:1.1.0")
+
     implementation("androidx.ui:ui-framework:$uiVersion")
     implementation("androidx.ui:ui-layout:$uiVersion")
     implementation("androidx.ui:ui-material:$uiVersion")
     implementation("androidx.ui:ui-tooling:$uiVersion")
+    implementation("androidx.compose:compose-runtime:$composeVersion")
+
     testImplementation("junit:junit:4.13")
     androidTestImplementation("androidx.test.ext:junit:1.1.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
